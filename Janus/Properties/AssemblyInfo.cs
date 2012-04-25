a7d1eaa7e9a332d3d2c93972adfe8351c2455331
andrey.chumak <andrexx@gmail.com>
2012-04-25 22:00:38 +0300

    Remove system.web settings from app config

--- Janus/Properties/AssemblyInfo.cs
+++ Janus/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Runtime.InteropServices;
 
 using Rsdn.Janus;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Janus")]
 [assembly: AssemblyDescription("Janus main application")]
 [assembly: AssemblyConfiguration("")]
@@ -15,20 +12,7 @@ using Rsdn.Janus;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//	  Major Version
-//	  Minor Version 
-//	  Build Number
-//	  Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion(ApplicationInfo.VersionString)]
 [assembly: AssemblyFileVersion(ApplicationInfo.VersionString)]
