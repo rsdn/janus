5317f86fa62087b135c87601d2d45278b0fc1248
andrewvk <avk@rsdn.ru>
2014-09-08 18:25:02 +0400

    Admin plugin improvements, do not copy common dlls in Rsdn plugin

--- Rsdn/Janus.Rsdn.Admin/Commands/AdminCommandTarget.cs
+++ Rsdn/Janus.Rsdn.Admin/Commands/AdminCommandTarget.cs
@@ -13,25 +13,34 @@ namespace Rsdn.Janus.Admin.Commands
 		private const string _modUrlTemplate = _adminUrl + "/ModerateMessage/{0}";
 		private const string _openReasonsEditorUrl = _adminUrl + "/PenaltyReasons";
 		private const string _openViolationRepsUrl = _adminUrl + "/ViolationReports";
+		private const string _editUrlTemplate = "http://rsdn.ru/Forum/NewMsg.aspx?mid={0}&edit=1";
 
 		public AdminCommandTarget(IServiceProvider serviceProvider) : base(serviceProvider)
 		{}
 
 		[CommandExecutor("Janus.Rsdn.Admin.ModerateMessage")]
-		public void ExecuteOpenMessageInJBrowser(ICommandContext context, int? messageId)
+		public void ExecuteModMessage(ICommandContext context, int? messageId)
 		{
 			context.OpenUrlInBrowser(
 				_modUrlTemplate.FormatStr(
 					ForumCommandHelper.GetMessageId(context, messageId)));
 		}
 
+		[CommandExecutor("Janus.Rsdn.Admin.EditMessage")]
+		public void ExecuteEditMessage(ICommandContext context, int? messageId)
+		{
+			context.OpenUrlInBrowser(_editUrlTemplate.FormatStr(ForumCommandHelper.GetMessageId(context, messageId)));
+		}
+
 		[CommandStatusGetter("Janus.Rsdn.Admin.ModerateMessage")]
+		[CommandStatusGetter("Janus.Rsdn.Admin.EditMessage")]
 		public CommandStatus QueryMessageCommandStatus(ICommandContext context, int? messageId)
 		{
 			return ForumCommandHelper.GetSingleMessageCommandStatus(context, messageId);
 		}
 
 		[CommandStatusSubscriber("Janus.Rsdn.Admin.ModerateMessage")]
+		[CommandStatusSubscriber("Janus.Rsdn.Admin.EditMessage")]
 		public IDisposable SubscribeMessageCommandStatusChanged(IServiceProvider serviceProvider, Action handler)
 		{
 			return ForumCommandHelper.SubscribeMessageCommandStatusChanged(serviceProvider, handler);
